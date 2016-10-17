42c9a1312599727217a1325a7a7bebf0b545d64c
LukeDJ24 <lukedjones23@gmail.com>
2016-10-17 14:56:46 +0100

    Add files via upload

---
+++ enemy.py
@@ -0,0 +1,111 @@
+from items import *
+
+enemy_rogue_knight = {
+    "id": "knight",
+
+    "name": "Rogue Knight",
+
+    "weak": ["Fire", "Bludgeon"],
+
+    "resist": ["Slash", "Stab"],
+
+    "attack": 45,
+
+    "hp": 100,
+
+    "temp_hp": 100,
+
+    "drop": []
+    }
+
+enemy_castle_mage = {
+    "id": "mage",
+
+    "name": "Castle Mage",
+
+    "weak": ["Slash", "Stab", "Bludgeon"],
+
+    "resist": ["Fire"],
+
+    "attack": 60,
+
+    "hp": 30,
+
+    "temp_hp": 30,
+
+    "drop": []
+    }
+enemy_kobold = {
+    "id": "kobold",
+
+    "name": "Harambe's Kobold",
+
+    "weak": ["Slash"],
+
+    "resist": ["Fire"],
+
+    "attack": 10,
+
+    "hp": 30,
+
+    "temp_hp": 30,
+
+    "drop": []
+
+} 
+enemy_bandit = {
+    "id": "bandit",
+
+    "name": "Bandit",
+
+    "weak": ["Fire"],
+
+    "resist": ["Slash"],
+
+    "attack": 30,
+
+    "hp": 40,
+
+    "temp_hp": 40, 
+    
+    "drop": [item_fireball, item_bandage]
+
+} 
+enemy_rufian = {
+    "id": "rufian",
+
+    "name": "Rufian",
+
+    "weak": ["Fire"],
+
+    "resist": ["Slash"],
+
+    "attack": 30,
+
+    "hp": 40,
+
+    "temp_hp": 40, 
+    
+    "drop": [item_fireball, item_bandage]
+
+}
+enemy_test = {
+    "id": "test",
+
+    "name": "Test",
+
+    "weak": ["Fire"],
+
+    "resist": ["Slash"],
+
+    "attack": 30,
+
+    "hp": 40,
+
+    "temp_hp": 40, 
+    
+    "drop": [item_fireball, item_bandage]
+
+} 
+    
+    
---
+++ items.py
@@ -0,0 +1,195 @@
+item_dagger = {
+    "id": "dagger",
+
+    "name": "Dagger",
+
+    "description":
+    """Stabby stab""",
+
+    "type": "Stab",
+
+    "attack": 10,
+
+    "cost": 20
+    }
+
+item_helping_hand = {
+    "id": "helping_hand",
+
+    "name": "Helping Hand",
+
+    "description":
+    """ Use to get into areas you sometimes may not be ready for""",
+
+    "type": "None",
+
+    "cost": 60
+    }
+
+item_mace = {
+    "id": "mace",
+
+    "name": "Mace",
+
+    "description":
+    """Used to blugeon enemies""",
+
+    "type": "Bludgeon",
+
+    "attack": 30,
+
+    "cost": 20
+    }
+item_restoration = {
+    "id": "restore",
+
+    "name": "Restoration",
+
+    "description":
+    """Used to restore health""",
+
+    "type": "Heal",
+
+    "hp": 30,
+
+    "cost": 30
+    } 
+item_princess = {
+    "id": "princess",
+
+    "name": "Princess",
+
+    "description":
+    """ She's got Mario whipped""",
+
+    "type": "None"
+    }
+
+item_fireball = {
+    "id": "fireball",
+
+    "name": "Fireball",
+
+    "description":
+    """ its a fireball""",
+
+    "type": "Fire",
+
+    "attack": 50,
+
+    "cost": 200
+}
+
+item_bandage = {
+    "id": "bandage",
+
+    "name": "Bandage",
+
+    "description":
+    """its a bandage""",
+
+    "type": "Heal",
+
+    "hp": 50,
+
+    "cost": 30
+
+}
+
+item_wood_block = {
+    "id": "wood",
+
+    "name": "Wood",
+
+    "description":
+    """ Wood blockade init fam""",
+
+    "type": "None",
+    
+    "cost": 2 
+    }
+item_sword = {
+    "id": "sword",
+
+    "name": "Iron Sword",
+
+    "description":
+    """basic sword, everyone's got one of these lying around""",
+
+    "type": "Slash",
+
+    "attack": 20,
+
+    "cost": 20
+
+} 
+item_upg_sword = {
+    "id": "steelsword",
+
+    "name": "Steel Sword",
+
+    "description":
+    """its a sword you twat""",
+
+    "type": "Slash",
+
+    "attack": 50,
+    
+    "cost": 30
+
+    }
+item_axe = {
+    "id": "axe",
+
+    "name": "Axe",
+
+    "description":
+    """Axe for wood chop chop""",
+
+    "type": "Slash",
+
+    "attack": 20,
+    
+    "cost": 30 
+}
+item_chainmail = {
+    "id": "chainmail",
+
+    "name": "Chainmail Armour",
+
+    "description":
+    """Armour fashioned from metal chains""",
+
+    "type": "Armour",
+
+    "hp": 50,
+
+    "cost": 40 
+}
+item_potion = {
+    "id": "potion",
+
+    "name": "Potion",
+
+    "description":
+    """Its. A. Potion.""",
+
+    "type": "Heal",
+
+    "hp": 100, 
+
+    "cost": 40 
+}
+item_clairvoyence = {
+    "id": "clairvoyence",
+
+    "name": "Clairvoyence",
+
+    "description":
+    """It will show you the way""",
+
+    "type": "Use", 
+
+    "cost": 50 
+    }
+
